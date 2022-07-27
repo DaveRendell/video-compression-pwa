5dcfb440826d895e3aeef5b151bfccf517bab4b9
David Rendell <davidwrendell@gmail.com>
2022-07-27 07:25:10 +0100

    Render web app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# video-compression-pwa
+# Video compression pwa
 A progressive web app for compressing videos in browser
+
+## Development
+
+Download dependencies: `npm install`
+Run development server: `npm run dev`
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import App from "./components/app"
+import "./styles.css"
+
+const mountNode = document.getElementById("app")
+const root = createRoot(mountNode)
+
+root.render(App())
