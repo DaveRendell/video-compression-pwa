87fc2a08add50b5ca3936ac2bb9af8c5035c9473
David Rendell <davidwrendell@gmail.com>
2022-08-02 22:03:48 +0100

    Add explanatory message

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,11 +7,16 @@ export default function App() {
   const reset = () => setSourceVideoFile(null)
 
   return (
-    <>
+    <main>
       <h1>Squishie</h1>
+      Squishes videos down to less than 8MB, to get around a certain messaging app's
+      file restrictions.
+      <br/>
+      All processing is done on your device, your videos won't
+      get sent to any servers.
       { !sourceVideoFile
           ? <FileUploader setSourceVideoFile={setSourceVideoFile}/>
           : <VideoProcessor sourceFile={sourceVideoFile} reset={reset} /> }
-    </>
+    </main>
   )
 }
